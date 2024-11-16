7738431fd97e2ff2ca03cdb26adff0f36cc4aeed
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-11-16 17:59:02 +0530

    sick

---
+++ configs/nvim/lua/settings.lua
@@ -0,0 +1,16 @@
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.softtabstop = 4
+vim.opt.mouse = "a"
+vim.opt.encoding = "utf-8"
+vim.opt.completeopt = vim.o.completeopt:gsub("preview", "")
+
+vim.g.NERDTreeDirArrowExpandable = "+"
+vim.g.NERDTreeDirArrowCollapsible = "~"
+vim.g.NERDTreeWinPos = "right"
+vim.g.NERDTreeWinSize = 50
+
+vim.keymap.set('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
