fd8c8c559b04649b70cd735b9560628ca3e1f480
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-11-16 15:35:59 +0530

    like this structured way of the nvim config much more!

---
+++ nvim/init.lua
@@ -0,0 +1,2 @@
+require("plugin_manager")
+require("settings")
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,14 @@
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.softtabstop = 4
+vim.opt.mouse = "a"
+vim.opt.encoding = "utf-8"
+vim.opt.completeopt = vim.o.completeopt:gsub("preview", "")
+
+vim.g.NERDTreeDirArrowExpandable = "+"
+vim.g.NERDTreeDirArrowCollapsible = "~"
+
+vim.keymap.set('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
