78c6d0c23846d7788f41ca5870b22fbd02e63fc9
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-11-16 15:26:50 +0530

    lets just use splitting for lazy.nvim stuff

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,6 +1,1 @@
 require("jonak")
-
-vim.g.NERDTreeDirArrowExpandable = "+"
-vim.g.NERDTreeDirArrowCollapsible = "~"
-
-vim.keymap.set('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
