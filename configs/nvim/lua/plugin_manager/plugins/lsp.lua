5992e762a5f54a31048bc4bdbbbcd1679cc521fe
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-11-25 17:37:14 +0530

    loving vim macros lol

--- configs/nvim/lua/plugin_manager/plugins/lsp.lua
+++ configs/nvim/lua/plugin_manager/plugins/lsp.lua
@@ -77,9 +77,9 @@ return {
 				["<C-Space>"] = cmp.mapping.complete(),
 			}),
 			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
+				{ name = "nvim_lsp" },
 			}, {
-				{ name = 'buffer' },
+				{ name = "buffer" },
 			})
 		})
 
