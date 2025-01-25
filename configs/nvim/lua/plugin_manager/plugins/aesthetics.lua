76b221c0c6abddc4895bb99ead761c0263d8554b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2025-01-25 06:58:49 +0000

    ooh wsl goes crazy

--- configs/nvim/lua/plugin_manager/plugins/aesthetics.lua
+++ configs/nvim/lua/plugin_manager/plugins/aesthetics.lua
@@ -1,32 +1,28 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme tokyonight-night]])
-		end,
-	},
-	{
-		"vim-airline/vim-airline",
-	},
-	{
-		"ryanoasis/vim-devicons",
-		priority = 500,
-	},
-	{
-		"preservim/nerdtree",
-	},
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme tokyonight-night]])
+        end,
+    },
+    {
+        "vim-airline/vim-airline",
+    },
+    {
+        "ryanoasis/vim-devicons",
+        priority = 500,
+    },
+    {
+        "preservim/nerdtree",
+    },
 
-	{
-		"ap/vim-css-color",
-	},
-	{
-		"terryma/vim-multiple-cursors",
-		lazy = true,
-	},
-	{
-		"wakatime/vim-wakatime",
-		lazy = false
-	}
+    {
+        "ap/vim-css-color",
+    },
+    {
+        "terryma/vim-multiple-cursors",
+        lazy = true,
+    },
 }
